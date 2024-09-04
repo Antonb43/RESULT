1e700b247f126a3e4f88d699cbc65504c7c977b1
AntonC <antonchernyshevv93@gmail.com>
2024-09-04 23:17:35 +0300

    Код программы написан

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,1 +1,26 @@
-﻿string [] array = {"Hi","Gee","Brains", ":-)", "Hello World"};
+﻿string[] array = { "Hi", "Gee", "Brains", ":-)", "Hello World" };
+
+int count = 0;
+foreach (string symbol in array)
+{
+    if (symbol.Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] result = new string[count];
+
+int index = 0;
+
+foreach (string symbol in array)
+{
+    if (symbol.Length <= 3)
+    {
+        result[index] = symbol;
+        index++;
+    }
+}
+
+Console.WriteLine("Новый массив из строк, длина которых меньше, либо равна 3 символам:");
+Console.WriteLine("[" + string.Join(", ", result) + "]");
